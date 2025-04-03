8b4d862897828d281997fc06b532641661b93660
aimanalishezan <aimanalishezan22@gmail.com>
2025-04-04 00:41:55 +0600

    added security

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+#import all the libs
 import gradio as gr
 import ollama
+import os
+from dotenv import load_dotenv
 
+# load security keys
+user=os.getenv('id')
+pas=os.getenv('pass')
+#load the model
 MODEL="CognitiveComputations/dolphin-llama3.1"
-
+#give system prompt
 system_message="You are a helpful AI Assistant. If you find any question asking about who you are like ' What's your name? ' , say 'My name is Man.Ai and my creator is Sir Ai-man and I develop by the team called Ai-ManS'"
-
+#crate function for chatinterface 
 def chat(message, history):
     messages=[{"role":"system","content":system_message}] + history + [{"role":"user","content":message}]
 
@@ -17,5 +24,5 @@ def chat(message, history):
     for chunk in stream:
         response += chunk["message"]["content"] if "message" in chunk else ""
         yield response
-
-gr.ChatInterface(fn=chat,type="messages").launch(share=True,pwa=True)
+#secure the link with id and pass with auth function
+gr.ChatInterface(fn=chat,type="messages").launch(share=True,pwa=True,auth=("user", "pas"))
